4782f08df31e79520c3d12b08b9a93a090add3fd
hmitchell-byui <hm1tch3ll99@gmail.com>
2026-04-03 20:44:47 -0500

    pathway adjustments

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,8 +2,8 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const utilities = require("../utilities")
 
-// Line 1 - a comment for the lines to follow.
 // Line 2 - brings Express into the scope of the file.
 // Line 3 - uses Express to create a new Router object. Remember in lesson 2 that using separate router files for specific elements of the application would keep the server.js file smaller and more manageable? That's what we're doing.
 // Line 4 - brings the inventory controller into this router document's scope to be used
@@ -13,7 +13,6 @@ router.get("/type/:classificationId", invController.buildByClassificationId);
 
 module.exports = router;
 
-// Line 11 - a comment for the route.
 // Line 12 - the route, which is divided into three elements:
 // "get" indicates that the route will listen for the GET method within the request (typically a clicked link or the URL itself).
 // /type/:classificationId the route being watched for (note that the inv element of the route is missing, but it will be accounted for later).
